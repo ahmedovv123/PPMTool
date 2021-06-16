2c05add903b457d59f8fb516b7613cefb883b1e7
ahmedovv123 <ahmedovv123@gmail.com>
2021-06-16 16:51:56 +0300

    find-all-projects

--- PPMToolFullStack/src/main/java/com/example/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/example/ppmtool/services/ProjectService.java
@@ -22,4 +22,19 @@ public class ProjectService {
             throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase()+"' already exists");
         }
     }
+
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null) {
+            throw new ProjectIdException("Project ID '" + projectId.toUpperCase()+"' does not exist");
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
 }
