9843affdc89cc1116ea2f6e85f6001e6523f2837
ahmedovv123 <ahmedovv123@gmail.com>
2021-06-16 18:34:48 +0300

    client-side

--- PPMToolFullStack/src/main/java/com/example/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/example/ppmtool/web/ProjectController.java
@@ -46,4 +46,13 @@ public class ProjectController {
     public Iterable<Project> getAllProject(){
         return projectService.findAllProjects();
     }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId){
+
+        projectService.deleteProjectByIdentifier(projectId);
+
+        return new ResponseEntity<String>("Project with ID: '" + projectId.toUpperCase() + "' was deleted !", HttpStatus.OK);
+    }
+
 }
