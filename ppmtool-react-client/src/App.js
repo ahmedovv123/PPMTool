7a26cf763011ef10cd86ba3cf8da597b447fa199
ahmedovv123 <ahmedovv123@gmail.com>
2021-06-21 11:56:39 +0300

    Crud operations

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, } from "react-router-dom";
+import AddProject from './components/Project/AddProject';
+import {Provider} from "react-redux";
+import store from "./store";
+import UpdateProject from './components/Project/UpdateProject';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store = {store}>
+      <Router>
+        <div className="App">
+          <Header />  
+          <Route exact path = "/dashboard" component = {Dashboard} />
+          <Route exact path = "/addProject" component = {AddProject} />
+          <Route exact path = "/updateProject/:id" component = {UpdateProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
